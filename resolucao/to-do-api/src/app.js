327af9812f0d6fb9613dc949ff7acb8feb8d1110
Josélia L. Costa <joselialcosta@gmail.com>
2019-11-04 16:10:35 -0300

    Add estrutura básica projeto resolvido

---
+++ resolucao/to-do-api/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+module.exports = app
